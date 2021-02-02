e54f7d7636c240d6f7f3e42bb8a86b1fb8f89c47
Daniel <wdaniel.jimenez@gmail.com>
2021-02-02 12:11:00 -0600

    Primeros ejercicios

---
+++ printFunction.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    a=''
+    for i in range(n):
+        a+=str(i+1)
+    print(a) 
---
+++ python_If-Else.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n%2 == 0:
+        if n>5 and n<21:
+            print('Weird')
+        else:
+            print('Not Weird')
+    else:
+        print('Weird')
